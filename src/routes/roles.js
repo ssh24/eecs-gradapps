69e9031b840e186666395826706e77f0379385b3
eddyv <edward.eddy.vaisman@gmail.com>
2018-02-02 02:54:35 -0500

    Added admin page with its respective css changes

--- src/routes/roles.js
+++ src/routes/roles.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('roles', { title: 'Role Selection'});
 });
 
+/* GET Admin Page */
+router.get('/admin', function(req,res, next){
+    res.render('admin', { title: 'Administrator'});
+})
+
 module.exports = router;
