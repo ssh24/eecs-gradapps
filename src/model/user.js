1f93a69bd61cd9f89360efe6de32d662a726693a
eddyv <edward.eddy.vaisman@gmail.com>
2018-02-21 14:58:55 -0500

    Added filtering options such as: - Selecting Columns - Filtering by Name, Field of Interest, Preferred Professor, Committee Ranking, Degree Applied For, Visa Status, My Interest Status. TODO: - Presets, Application Status (Do we break this up or just leave it all in this option?), GPA (How to search for range when incorporating +'s?) - Update selected filter on client side - Notification for when a result turns no tables - TESTS

--- src/model/user.js
+++ src/model/user.js
@@ -8,53 +8,53 @@ var endOfLine = require('os').EOL;
 // this file path can be pointed to any server located file or local file for htpasswd to work
 // for security reasons, all htpasswd should be bycrypted
 var htpasswdFile = path.resolve(__dirname, '..', '.private', '.htpasswd');
-     
+
 var User = function() {};
 
 /**
  * Find a user by the username.
- * @param {String} username 
- * @param {Function} cb 
+ * @param {String} username
+ * @param {Function} cb
  */
 User.prototype.findUser = function(username, cb) {
-	var lines;
-	var user = null;
-	var data = fs.readFileSync(htpasswdFile, 'utf8');
-	lines = data.split('\n');
-	for(var i = 0; i < lines.length; i++) {
-		var line = lines[i];
-		if(line) {
-			if (line.split(':')[0] === username) {
-				user = username;
-				break;
-			}
-		}
-	}
-	return cb(null, user);
+  var lines;
+  var user = null;
+  var data = fs.readFileSync(htpasswdFile, 'utf8');
+  lines = data.split('\n');
+  for (var i = 0; i < lines.length; i++) {
+    var line = lines[i];
+    if (line) {
+      if (line.split(':')[0] === username) {
+        user = username;
+        break;
+      }
+    }
+  }
+  return cb(null, user);
 };
 
 /**
  * Check if a password is correct for the specific user.
- * @param {String} username 
- * @param {HashedString} password 
+ * @param {String} username
+ * @param {HashedString} password
  */
 User.prototype.validPassword = function(username, password) {
-	var lines, user, pass;
-	var found = false;
-	var data = fs.readFileSync(htpasswdFile, 'utf8');
-	lines = data.split(endOfLine);
-	for(var i = 0; i < lines.length; i++) {
-		var line = lines[i];
-		if(line) {
-			user = line.split(':')[0];
-			pass = line.split(':')[1].trim();
-			if (user === username) {
-				found = (pass === md5(password, pass));
-				break;
-			}
-		}
-	}
-	return found;
+  var lines, user, pass;
+  var found = false;
+  var data = fs.readFileSync(htpasswdFile, 'utf8');
+  lines = data.split(endOfLine);
+  for (var i = 0; i < lines.length; i++) {
+    var line = lines[i];
+    if (line) {
+      user = line.split(':')[0];
+      pass = line.split(':')[1].trim();
+      if (user === username) {
+        found = (pass === md5(password, pass));
+        break;
+      }
+    }
+  }
+  return found;
 };
 
 module.exports = User;
