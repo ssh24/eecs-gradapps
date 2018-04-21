d1bc0e4c322226b5b245d23643f359bb0b0bd8f1
ssh24 <sshasan10@hotmail.com>
2018-04-20 20:41:56 -0400

    Init filtering on reviews

--- src/public/js/manage-review.js
+++ src/public/js/manage-review.js
@@ -44,17 +44,13 @@ $(document).ready(function() {
             var filterText = '';
             //change the below for gpa and committe once merged.
             for (var i = 0; i < filters.length; i++) {
-                if (filters[i] === "btn_filter_name") {
-                  filterText += "Applicant Name = ";
-                } else if (filters[i] === "btn_filter_degree") {
-                  filterText += "Degree Applied For = ";
-                } else if (filters[i] === "btn_filter_gpa") {
-                  filterText += "GPA ";
-                } else if (filters[i] === "btn_filter_visa") {
+                if (filters[i] === "btn_filter_visa") {
                     filterText += "Visa Status = "
-                } else if (filters[i] === "btn_filter_program_decision") {
-                    filterText += "Program Decision = "
-                }
+                } else if (filters[i] === "btn_filter_foi") {
+                    filterText += "Field(s) of Interest = "
+                } else if (filters[i] === "btn_filter_prof") {
+                    filterText += "Preferred Professor(s) = "
+                } 
                 filterText += $("#" + filters[i]).find("option:selected").text();
                 if (i + 1 < filters.length) {
                   filterText += " <span style=\"color:red\">AND</span> ";
@@ -64,7 +60,7 @@ $(document).ready(function() {
         });
     
         $("#filter_submit").click(function(e) {
-            var action = "/roles/admin/applications/filter";
+            var action = "/roles/admin/reviews/filter";
         
             $("#filterForm").attr('action', action);
             for (var i = 0; i < colOrder.length; i++) {
@@ -73,70 +69,5 @@ $(document).ready(function() {
             //submit the form with the selected columns.
             $("#filterForm").submit();
             e.preventDefault();
-        });
-    
-        $("#save_preset").click(function(e) {
-            if ($.trim($("#preset_name").val())) {
-                var action = "/roles/admin/applications/filter";
-                $("#filterForm").attr('action', action);
-        
-                for (var i = 0; i < colOrder.length; i++) {
-                    $(this).append('<input type="hidden" name="selectedCol[]" value="' + colOrder[i] + '">');
-                }
-                //submit the form with the selected columns.
-                $("#filterForm").submit();
-            } else {
-                alert("Please input a name for the preset!");
-            }
-            e.preventDefault();
-        });
-    
-        // update fields and columns
-        $("#preset").change(function() {
-            var key = $(this).val();
-            if (key !== "") {
-              var cols = presets[key]['cols'];
-              var filt = presets[key]['filt'];
-              colOrder = [];
-              for (var col in presets[key]['cols']) {
-                $("#" + col).removeClass("active");
-                //removes the span element.
-                $("#" + col).children().remove();
-                if (presets[key]['cols'][col] && presets[key]['cols'][col] !== '') {
-                  $("#" + col).addClass("active");
-                  colOrder[presets[key]['cols'][col]-1] = col;
-                  $("#" + col).append(" <span class='label label-primary'>" + presets[key]['cols'][col] + "</span>");
-                }
-              }
-              filters = [];
-              for (var filt in presets[key]['filt']) {
-                $("#" + filt).val(presets[key]['filt'][filt]);
-                $("#" + filt).selectpicker('render');
-                filters.push(filt);
-              }
-              //update string
-              var filterText = '';
-              //change the below for gpa and committe once merged.
-              for (var i = 0; i < filters.length; i++) {
-                if ($("#" + filters[i]).find("option:selected").text() !== '' && $("#" + filters[i]).find("option:selected").text() !== 'Any') {
-                  if(filterText.length > 0){
-                      filterText += " <span style=\"color:red\">AND</span> ";
-                  }
-                  if (filters[i] === "btn_filter_name") {
-                    filterText += "Applicant Name = ";
-                  } else if (filters[i] === "btn_filter_degree") {
-                    filterText += "Degree Applied For = ";
-                  } else if (filters[i] === "btn_filter_gpa") {
-                    filterText += "GPA ";
-                  } else if (filters[i] === "btn_filter_visa") {
-                      filterText += "Visa Status = "
-                  } else if (filters[i] === "btn_filter_program_decision") {
-                      filterText += "Program Decision = "
-                  }
-                  filterText += $("#" + filters[i]).find("option:selected").text();
-                }
-              }
-              $("#selectedFilter").html(filterText);
-            }
         });
 });
